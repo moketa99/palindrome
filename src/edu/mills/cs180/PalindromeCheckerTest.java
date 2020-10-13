68755e919dddad6716c8b9a82ef31cbb3db6d98e
Makie <mmaekawa@mills.edu>
2020-10-13 07:27:27 -0700

    Add tests

--- src/edu/mills/cs180/PalindromeCheckerTest.java
+++ src/edu/mills/cs180/PalindromeCheckerTest.java
@@ -24,4 +24,23 @@ class PalindromeCheckerTest {
     void isPalindrome_False_SingleWordPalindrome(String s) {
         assertFalse(PalindromeChecker.isPalindrome(s), "Wrong string " + s);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Ama", "Wow", "reddER"})
+    void isPalindrome_True_DifferingCase(String s) {
+        assertTrue(PalindromeChecker.isPalindrome(s));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"my gym", "top spot", "step on no pets"})
+    void isPalindrome_True_HavingSpaces(String s) {
+        assertTrue(PalindromeChecker.isPalindrome(s));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"WOW!!!", "I did, did I?", "?!"})
+    void isPalindrome_True_HavingPunctuationAndSpaces(String s) {
+        assertTrue(PalindromeChecker.isPalindrome(s));
+    }
+
 }
